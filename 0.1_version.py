22e9837bd6668082c79a0f4ddfabb39ab23dcf09
sudarshan-sudo <sudarshantata007@gmail.com>
2025-06-05 09:38:31 +0530

    First basic version of my Calculator

--- 0.1_version.py
+++ 0.1_version.py
@@ -1,1 +1,14 @@
-print ("hello world")
+x = int(input ("what is your first value"))
+y = int(input ("what is your second value"))
+
+opp = input("1: addition" "2: sub" "3: mulpti" "4: divide")
+
+if opp == "3":
+    print (x*y);
+if opp == "1":
+    print (x+y);
+if opp == "2":
+    print(x-y);
+if opp =="4":
+    print(x/y);
+
