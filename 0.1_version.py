fca7b2adc92066fcc96cd62ac87a56f8026fa1d3
sudarshan-sudo <sudarshantata007@gmail.com>
2025-06-05 16:53:07 +0530

    Latest version 5.0

--- 0.1_version.py
+++ 0.1_version.py
@@ -3,13 +3,13 @@ y = float(input ("what is your second value"))
 
 opp = input("1: addition" "2: sub" "3: mulpti" "4: divide")
 
-if opp == "3":
-    print (x*y);
+if opp == "4":
+    print (x/y);
 if opp == "1":
     print (x+y);
 if opp == "2":
     print(x-y);
-if opp =="4":
-    print(x/y)
+if opp =="3":
+    print(x*y)
     
 
